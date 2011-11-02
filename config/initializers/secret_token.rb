cccb1ec395bfeafffcdb8b6877a080682477ac81
Andrey Voitishin <a.voitishin@gmail.com>
2011-11-02 18:34:24 -0400

    rails new alpha -sT --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alpha::Application.config.secret_token = 'ddbd3d4776d944618f4b207a11ec6d35d70c0dc61b783c7f10a88779da974bd4f4b425a228ffa8499414181559c6bff3fa917c995db6236dc607f38c64860786'
