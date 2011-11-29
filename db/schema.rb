b172bf08365862bef1f7deffd3e08ecc40700542
Ryan Murphy <rdmurphy@gmail.com>
2011-11-29 17:13:05 -0500

    indexes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111129181336) do
+ActiveRecord::Schema.define(:version => 20111129220937) do
 
   create_table "domains", :force => true do |t|
     t.integer  "site_id"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(:version => 20111129181336) do
     t.string   "meta_author"
   end
 
+  add_index "pages", ["site_id"], :name => "index_pages_on_site_id"
+
   create_table "sites", :force => true do |t|
     t.string   "subdomain"
     t.datetime "created_at"
@@ -43,6 +45,8 @@ ActiveRecord::Schema.define(:version => 20111129181336) do
     t.string   "title"
     t.string   "theme"
   end
+
+  add_index "sites", ["subdomain"], :name => "index_sites_on_subdomain"
 
   create_table "users", :force => true do |t|
     t.string   "email",           :null => false
